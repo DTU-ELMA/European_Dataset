2a6f3155b0d4a30e6c594a6bb9588a2bc7079a8d
TueVJ <tue.vissing.jensen@gmail.com>
2015-08-17 16:30:09 +0200

    update defaults for signal conversion

--- Scripts/Convert_Signals/Myhelpers/defaults.py
+++ Scripts/Convert_Signals/Myhelpers/defaults.py
@@ -1,14 +1,14 @@
-startyear = 2014
-startmonth = 06
+startyear = 2012
+startmonth = 1
 endyear = 2014
-endmonth = 06
+endmonth = 12
 
 # Geographical files
 
-onshoremapfilename = '../../Data/Metadata/onshore.npy'
-
-latitudefile = '../../Data/Metadata/lats.npy'
-longitudefile = '../../Data/Metadata/lons.npy'
+onshoremapfilename = '../../Data/Metadata/onshore_ECMWF.npy'
+latitudefile = '../../Data/Metadata/lats_ECMWF.npy'
+longitudefile = '../../Data/Metadata/lons_ECMWF.npy'
+nodeorder = '../../Data/Metadata/nodeorder.npy'
 
 # Wind files
 
@@ -17,16 +17,14 @@ longitudefile = '../../Data/Metadata/lons.npy'
 onshoreturbinecfg = 'TurbineData/Siemens_SWT_107_3600kW_smoothed.cfg'
 offshoreturbinecfg = 'TurbineData/Siemens_SWT_107_3600kW_anholt_smoothed.cfg'
 
-windforecastdatadir = "../../Data/ECMWF-data/"
-windforecastoutdir = "../../Data/Nodal_Forecasts/"
+windsignaldatadir = "../../Data/Signal_Processed/"
+windsignaloutdir = "../../Data/Nodal_Signal/"
 
-windprojectionmatrix = '../../Data/Metadata/wndtransfercsr.npz'
-nodeorder = '../../Data/Metadata/nodeorder.npy'
-
+windprojectionmatrix = '../../Data/Metadata/wndtransfercsr_ECMWF.npz'
 
 # Solar files
-solarforecastdatadir = "../../Data/ECMWF-data/"
-solarforecastoutdir = "../../Data/Nodal_Forecasts/"
+solarsignaldatadir = "../../Data/Signal_Unprocessed/"
+solarsignaloutdir = "../../Data/Nodal_Signal/"
 
 solarpanelcfg = 'SolarPanelData/Scheuten215IG.cfg'
-solarprojectionmatrix = '../../Data/Metadata/solartransfercsr.npz'
+solarprojectionmatrix = '../../Data/Metadata/solartransfercsr_ECMWF.npz'
--- Scripts/Convert_Signals/convert_solar_signal.py
+++ Scripts/Convert_Signals/convert_solar_signal.py
@@ -26,8 +26,8 @@ def fix_solar(L):
 # Argument parser
 parser = argparse.ArgumentParser(description='Wind conversion options')
 
-parser.add_argument('-r', '--rootdir', help='Input directory for forecast files', default=defaults.solarforecastdatadir, metavar="forecast root")
-parser.add_argument('-o', '--outdir', help='Output directory for forecast files', default=defaults.solarforecastoutdir, metavar="forecast outroot")
+parser.add_argument('-r', '--rootdir', help='Input directory for signal files', default=defaults.solarsignaldatadir, metavar="signal root")
+parser.add_argument('-o', '--outdir', help='Output directory for signal files', default=defaults.solarsignaloutdir, metavar="signal outroot")
 
 parser.add_argument('-f', '--first', help='First year to extract', default=defaults.startyear, type=int, metavar="first year")
 parser.add_argument('-l', '--last', help='Last year to extract', default=defaults.endyear, type=int, metavar="last year")
@@ -45,10 +45,10 @@ lons = np.load(args.lonfile)
 # We have a forecast for each hour
 forecastdelta = datetime.timedelta(hours=1)
 
-# Temperature, downward solar and Albedo name
-tmpname = 'ctr_P167_LSFC'
-dsrname = 'ctr_P169_LSFC'
-albname = 'ctr_P243_LSFC'
+# Input file names
+dsolarfilename = 'dswsfc-{}.npz'
+usolarfilename = 'uswsfc-{}.npz'
+tmp2mfilename = 'tmp2m-{}.npz'
 
 # PV slope function:
 # Panel angled at 30 deg from horizontal
@@ -64,8 +64,9 @@ panelconfig = ConfigObj(args.solarpaneltypefile, list_values=False, configspec=c
 panelconfig.validate(Validator())
 
 # Set up filename
-filename = "PVpower_{0}.npz".format(panelconfig['name'])
-filename.replace(" ", "_")
+outfilename = "PVpower_{0}.npz".format(panelconfig['name'])
+outfilename.replace(" ", "_")
+outfilename += "-{0:04d}{1:02d}.npz"
 
 # Load matrix to project to nodal space
 solartransfer = np.load(defaults.solarprojectionmatrix)
@@ -75,65 +76,55 @@ solartransfer = sparse.csr_matrix((solartransfer['data'], solartransfer['indices
 nodeorder = np.load(defaults.nodeorder)
 
 # Select only the forecasts specified for conversion.
-forecastls = sorted(os.listdir(args.rootdir))
-startdate = '{0:04d}{1:02d}0100'.format(args.first, args.fm)
-stopdate = '{0:04d}{1:02d}0100'.format(args.last+int(args.lm == 12), args.lm % 12 + 1)
+signalls = sorted(os.listdir(args.rootdir))
+dswsfcls = [x for x in signalls if 'dswsfc' in x]
+uswsfcls = [x for x in signalls if 'uswsfc' in x]
+tmpls = [x for x in signalls if 'tmp2m' in x]
+
+startdate = '{0:04d}{1:02d}'.format(args.first, args.fm)
+stopdate = '{0:04d}{1:02d}'.format(args.last+int(args.lm == 12), args.lm % 12+1)
 try:
-    startidx = forecastls.index(startdate)
+    dsstartidx = dswsfcls.index(dsolarfilename.format(startdate))
+    usstartidx = uswsfcls.index(usolarfilename.format(startdate))
+    tmpstartidx = tmpls.index(tmp2mfilename.format(startdate))
 except ValueError:
-    print('Start month not found - check forecast directory')
+    print('Start month not found - check directory')
     raise ValueError
 try:
-    stopidx = forecastls.index(stopdate)
-    forecastls = forecastls[startidx:stopidx]
+    dsstopidx = dswsfcls.index(dsolarfilename.format(stopdate))
+    usstopidx = uswsfcls.index(usolarfilename.format(stopdate))
+    tmpstopidx = tmpls.index(tmp2mfilename.format(stopdate))
+    dswsfcls = dswsfcls[dsstartidx:dsstopidx]
+    uswsfcls = uswsfcls[usstartidx:usstopidx]
+    tmpls = tmpls[tmpstartidx:tmpstopidx]
 except ValueError:
-    print 'Stopmonth+1 not found - assuming we need to use all directories'
-    forecastls = forecastls[startidx:]
+    print 'Stopdate + 1 month not found - assuming we need to use all time series'
+    dswsfcls = dswsfcls[dsstartidx:]
+    uswsfcls = uswsfcls[usstartidx:]
+    tmpls = tmpls[tmpstartidx:]
 
 # MAIN LOOP
 # For each forecast:
-# - Extract fields of downward radiation, albedo and temperature
-# - Calculate upward radiation
+# - Load fields of downward radiation, upward radiation and temperature
 # - Convert to capacity factors
 # - Project to nodal domain
 # - Save nodal forecast time series
-for fdir in forecastls:
-    print fdir
-    date = parse_datetime_string(fdir)
-
-    # Load files
-    dsfile = pg.open(args.rootdir + fdir + '/' + dsrname)
-    albfile = pg.open(args.rootdir + fdir + '/' + albname)
-    tmpfile = pg.open(args.rootdir + fdir + '/' + tmpname)
-    # dsdata_int = np.array([ds['values'] for ds in dsfile])
-    # albdata = np.array([alb['values'] for alb in albfile])
-    # tmpdata = np.array([tmp['values'] for tmp in tmpfile])
-    dsdata_int = np.array([ds['values'][::-1] for ds in dsfile])
-    albdata = np.array([alb['values'][::-1] for alb in albfile])
-    tmpdata = np.array([tmp['values'][::-1] for tmp in tmpfile])
-
-    dsdata = fix_solar(dsdata_int)
-    usdata = dsdata * albdata
-
-    dates = [date + forecastdelta*i for i in range(len(dsdata))]
-
-    # solar conversion
+for dsolarfile, usolarfile, tmp2mfile in zip(dswsfcls, uswsfcls, tmpls):
+    print dsolarfile
+    dsfile = np.load(args.rootdir + '/' + dsolarfile)
+    usfile = np.load(args.rootdir + '/' + usolarfile)
+    tmpfile = np.load(args.rootdir + '/' + tmp2mfile)
+    dsdata = dsfile['data']
+    usdata = usfile['data']
+    tmpdata = tmpfile['data']
+    dates = dsfile['dates']
     convdata = np.zeros_like(dsdata)
-
     for influx, outflux, tmp2m, utcTime, outidx in zip(dsdata, usdata, tmpdata, dates, range(len(convdata))):
         influx_tilted = newHayDavies(influx, outflux, lats, lons, utcTime, slopefunction)
         out = SolarPVConversion((influx_tilted, tmp2m), panelconfig)
         out /= (panelconfig['rated_production']/panelconfig['area'])
         convdata[outidx] = np.nan_to_num(out)
-
-    raise SystemExit
-    # Projection to nodal domain
     shape = convdata.shape
     outdata = solartransfer.dot(np.reshape(convdata, (shape[0], shape[1]*shape[2])).T).T
 
-    # Save .npy file
-    try:
-        np.savez_compressed(args.outdir + '/' + fdir + '/' + filename, data=outdata, dates=dates)
-    except IOError:
-        os.mkdir(args.outdir + '/' + fdir + '/')
-        np.savez_compressed(args.outdir + '/' + fdir + '/' + filename, data=outdata, dates=dates)
+    np.savez_compressed(args.outdir + '/' + outfilename.format(dates[0].year, dates[0].month), data=outdata, dates=dates)
