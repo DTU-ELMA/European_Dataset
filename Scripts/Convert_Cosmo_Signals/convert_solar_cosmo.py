0185b129986a6c5c37c8c07bc3a8f18c99ddff0f
TueVJ <tue.vissing.jensen@gmail.com>
2015-09-29 13:55:12 -0400

    Update solar conversion - Hdf5, remove testing code, enable partial conversions

--- Scripts/Convert_Cosmo_Signals/convert_solar_cosmo.py
+++ Scripts/Convert_Cosmo_Signals/convert_solar_cosmo.py
@@ -2,6 +2,7 @@
 import os
 import pygrib as pg
 import numpy as np
+import pandas as pd
 import argparse
 import Myhelpers.defaults as defaults
 import scipy.sparse as sparse
@@ -12,15 +13,6 @@ from validate import Validator
 from itertools import izip as zip
 from Myhelpers import write_datetime_string, parse_datetime_string
 from datetime import timedelta
-
-def fix_solar(L):
-    '''
-    Integrated field to hourly average values.
-    Divides by 3600 to convert joule = watthr to watthr/hr.
-    '''
-    out = [x/3600 for x in np.diff(L, axis=0)]
-    out.append(L[0]/3600)
-    return np.array(out)
 
 
 # Argument parser
@@ -34,8 +26,7 @@ parser.add_argument('-l', '--last', help='Last year to extract', default=default
 parser.add_argument('-fm', help='First month to extract', default=defaults.startmonth, type=int, metavar="first month")
 parser.add_argument('-lm', help='Last month to extract', default=defaults.endmonth, type=int, metavar="last month")
 parser.add_argument('-sp', '--solarpaneltypefile', metavar="solarpaneltypefile", type=str, help='File containing characteristics of the solar panel to use', default=defaults.solarpanelcfg)
-parser.add_argument('-la', '--latfile', help='Latitude file', default=defaults.latitudefile, type=str, metavar="latfile")
-parser.add_argument('-lo', '--lonfile', help='Longitude file', default=defaults.longitudefile, type=str, metavar="lonfile")
+parser.add_argument('-md', '--halfheightfile', help='Half-height layers file', default=defaults.halfheightfile, type=str, metavar="halfheightfile")
 
 args = parser.parse_args()
 
@@ -67,59 +58,65 @@ solartransfer = sparse.csr_matrix((solartransfer['data'], solartransfer['indices
 # load nodeorder file (used for saving)
 nodeorder = np.load(defaults.nodeorder)
 
-fH = pg.open('metadata/HHL.grb')
-
+# Load half-height data, latitude and longitude
+fH = pg.open(args.halfheightfile)
 lats, lons = fH[1].latlons()
 
 # Select only the forecasts specified for conversion.
 fdir = sorted(os.listdir(args.rootdir))
 
-startdate = '{0:04d}{1:02d}0100'.format(args.first, args.fm)
-stopdate = '{0:04d}{1:02d}0100'.format(args.last+int(args.lm == 12), args.lm % 12 + 1)
+startdate = 'laf{0:04d}{1:02d}01000000'.format(args.first, args.fm)
+stopdate = 'laf{0:04d}{1:02d}01000000'.format(args.last+int(args.lm == 12), args.lm % 12 + 1)
 
-# try:
-#     startidx = forecastls.index(startdate)
-# except ValueError:
-#     print('Start month not found - check forecast directory')
-#     raise ValueError
-# try:
-#     stopidx = forecastls.index(stopdate)
-#     forecastls = forecastls[startidx:stopidx]
-# except ValueError:
-#     print 'Stopmonth+1 not found - assuming we need to use all directories'
-#     forecastls = forecastls[startidx:]
+try:
+    startidx = fdir.index(startdate)
+except ValueError:
+    print('Start month not found - check forecast directory')
+    raise ValueError
+try:
+    stopidx = fdir.index(stopdate)
+    fdir = fdir[startidx:stopidx]
+except ValueError:
+    print 'Stopmonth+1 not found - assuming we need to use all directories'
+    fdir = fdir[startidx:]
 
 # MAIN LOOP
 # For each forecast:
-# - Extract fields of downward radiation, albedo and temperature
-# - Calculate upward radiation
+# - Extract fields of upward, downward, and ground reflected radiation and temperature
 # - Convert to capacity factors
 # - Project to nodal domain
 # - Save nodal forecast time series
 
-albold =0
+albold = 0
 tmpold = 0
 Ibold = 0
 Idold = 0
 Igold = 0
 
+converted_series = []
+times = []
 for cosmo_file in (x for x in fdir if x[0] != "."):
 
     print cosmo_file
 
     date = parse_datetime_string(cosmo_file)
-    previous = date + timedelta(hours = -1)
+    previous = date + timedelta(hours=-1)
 
     hour = previous.hour
 
     # Load file
     f = pg.open(args.rootdir + cosmo_file)
     d = [x.values for x in f]
-    alb = np.array(d[13]*(hour%6+1) - albold*(hour%6)).clip(0)
-    tmp = np.array(d[14]*(hour%6+1) - tmpold*(hour%6)).clip(0)
-    Ib = np.array(d[17]*(hour%6+1) - Ibold*(hour%6)).clip(0)
-    Id = np.array(d[18]*(hour%6+1) - Idold*(hour%6)).clip(0)
-    Ig = np.array(d[19]*(hour%6+1) - Igold*(hour%6)).clip(0)
+    # Albedo
+    alb = np.array(d[13]*(hour % 6+1) - albold*(hour % 6)).clip(0)
+    # Temperature
+    tmp = np.array(d[14]*(hour % 6+1) - tmpold*(hour % 6)).clip(0)
+    # Irradiation beam
+    Ib = np.array(d[17]*(hour % 6+1) - Ibold*(hour % 6)).clip(0)
+    # Irradiation diffuse
+    Id = np.array(d[18]*(hour % 6+1) - Idold*(hour % 6)).clip(0)
+    # Irradiation ground
+    Ig = np.array(d[19]*(hour % 6+1) - Igold*(hour % 6)).clip(0)
 
     albold = d[13]
     tmpold = d[14]
@@ -127,38 +124,27 @@ for cosmo_file in (x for x in fdir if x[0] != "."):
     Idold = d[18]
     Igold = d[19]
 
-    # dsfile = pg.open(args.rootdir + fdir + '/' + dsrname)
-    # albfile = pg.open(args.rootdir + fdir + '/' + albname)
-    # tmpfile = pg.open(args.rootdir + fdir + '/' + tmpname)
-    # dsdata_int = np.array([ds['values'] for ds in dsfile])
-    # albdata = np.array([alb['values'] for alb in albfile])
-    # tmpdata = np.array([tmp['values'] for tmp in tmpfile])
-    # dsdata_int = np.array([ds['values'][::-1] for ds in dsfile])
-    # albdata = np.array([alb['values'][::-1] for alb in albfile])
-    # tmpdata = np.array([tmp['values'][::-1] for tmp in tmpfile])
-
-    #dsdata = fix_solar(dsdata_int)
-    #usdata = dsdata * albdata
-
-    #dates = [date + forecastdelta*i for i in range(len(dsdata))]
-
     # solar conversion
-    #convdata = np.zeros_like(dsdata)
-
-    #influx, outflux, tmp2m, utcTime, outidx = dsdata, usdata, tmpdata, dates, range(len(convdata)))
     influx_tilted = newHayDavies(Ib, Id, Ig, lats, lons, date, slopefunction)
     out = SolarPVConversion((influx_tilted, tmp), panelconfig)
     out /= (panelconfig['rated_production']/panelconfig['area'])
 
-    #convdata[outidx] = np.nan_to_num(out)
+    # NaN's crop up for high latitudes in summer months
+    # when the sun shines on the reverse face of the panel
+    out = np.nan_to_num(out)
 
     # Projection to nodal domain
     shape = out.shape
     outdata = solartransfer.dot((out.flatten()).T).T
 
-    # Save .npy file
-    try:
-        np.savez_compressed(args.outdir + '/' + str(date.year) + '_' + str(date.month) + '_' + str(date.day) + '_' + str(date.hour) + '_' + filename, data=outdata, dates=date)
-    except IOError:
-        os.mkdir(args.outdir + '/')
-        np.savez_compressed(args.outdir + '/' + str(date.year) + '_' + str(date.month) + '_' + str(date.day) + '_' + str(date.hour) + '_' + filename, data=outdata, dates=date)
+    converted_series.append(outdata)
+    times.append(date)
+
+outdf = pd.DataFrame(data=converted_series, index=times, columns=nodeorder)
+store = pd.HDFStore(defaults.windoutdir + 'COSMO-store.h5')
+try:
+    olddf = store['COSMO/solar']
+    store['COSMO/solar'] = outdf.combine_first(olddf)
+except KeyError:
+    store['COSMO/solar'] = outdf
+store.close()
