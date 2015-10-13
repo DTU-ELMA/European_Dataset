95ed7b70fcd7d8ddb447e23f4dd9e6b9c1daa88f
TueVJ <tue.vissing.jensen@gmail.com>
2015-10-13 14:20:42 -0400

    comments

--- Scripts/Project_to_Nodal/build_nodal_projection_matrix.py
+++ Scripts/Project_to_Nodal/build_nodal_projection_matrix.py
@@ -4,6 +4,7 @@ from latlon_to_spatial import latlonstospace
 from scipy.spatial import KDTree
 import scipy.sparse as sparse
 
+# Location of metadata files
 metadatadir = '../../Data/Metadata/'
 
 nodeorder = np.load(metadatadir + 'nodeorder.npy')
@@ -12,15 +13,17 @@ solarmask = np.load(metadatadir + 'solarmask_ECMWF.npy').astype(bool)
 lats = np.load(metadatadir + 'lats_ECMWF.npy')
 lons = np.load(metadatadir + 'lons_ECMWF.npy')
 
+# Load lat/lons, convert to points in 3D space (Closest point on greatcircle metric = closest point in spatial metric.).
 windgridpos = np.array([lons[windmask], lats[windmask]]).T
 windgridspos = latlonstospace(windgridpos[:, 1], windgridpos[:, 0])
 
 solargridpos = np.array([lons[solarmask], lats[solarmask]]).T
 solargridspos = latlonstospace(solargridpos[:, 1], solargridpos[:, 0])
 
-# ENTSOE grid data
+# Load network data
 entsoe_grid = nx.read_gpickle(metadatadir + 'network_postfit.gpickle')
 nodepos = nx.get_node_attributes(entsoe_grid, 'pos')
+# Convert node lat/lons to points in 3D space.
 nodepos = np.array([nodepos[n] for n in nodeorder])
 nodespos = latlonstospace(nodepos[:, 1], nodepos[:, 0])
 
@@ -54,7 +57,7 @@ for i, j in zip(nindx, sindx):
     wndtransferbig[i, j] /= ssum[j]
 
 
-# # Load transfer matrix should preserve sum of incoming squares.
+# # Load transfer matrix preserves sum of incoming signal.
 loadtransferout = wndtransferbig.tocsr()
 np.savez(
     metadatadir + 'loadtransfercsr_ECMWF.npz',
@@ -69,7 +72,7 @@ nsum = np.squeeze(np.asarray(wndtransferbig.sum(1)))
 for i, j in zip(nindx, sindx):
     wndtransferbig[i, j] /= nsum[i]
 
-# # Wind transfer matrix should preserve average of incoming squares.
+# # Wind transfer matrix should preserve average of incoming signal.
 wndtransferout = wndtransferbig.tocsr()
 
 np.savez(
@@ -79,7 +82,9 @@ np.savez(
     indptr=wndtransferout.indptr,
     shape=wndtransferout.shape)
 
-# to load:
+# to load a projection matrix:
+# import numpy as np
+# from scipy import sparse
 # wndtransfer = np.load(metadatadir + 'wndtransfercsr.npz')
 # wndtransfer = sparse.csr_matrix((wndtransfer['data'],  wndtransfer['indices'],  wndtransfer['indptr']),  shape=wndtransfer['shape'])
 
@@ -124,6 +129,6 @@ np.savez(
     indptr=solartransferout.indptr,
     shape=solartransferout.shape)
 
-# to load:
+# to load this matrix:
 # solartransfer = np.load(metadatadir + 'solartransfercsr.npz')
 # solartransfer = sparse.csr_matrix((solartransfer['data'],  solartransfer['indices'],  solartransfer['indptr']),  shape=solartransfer['shape'])
